73e4003ec4fe28403d995ca4b25b69b75d0e50e9
Ana Paola Oviedo <anapaolaoviedo@Anas-MacBook-Pro.local>
2025-06-13 11:43:17 -0600

    fourth commit

--- lib/jswatch_web/statecharts/clock_manager.ex
+++ lib/jswatch_web/statecharts/clock_manager.ex
@@ -38,4 +38,33 @@ defmodule JswatchWeb.ClockManager do
     end
   end
 
+
+    # ticking even in alarmoff state
+  def handle_info(:working_working, %{ui_pid: ui, time: time, st: :alarm_off} = state) do
+    Process.send_after(self(), :working_working, 1000)
+    time = Time.add(time, 1)
+    GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string()})
+    {:noreply, %{state | time: time}}
+  end
+
+
+   def handle_info(:working_working, %{ui_pid: ui, time: time, alarm: alarm, st: :snooze_on} = state) do
+    Process.send_after(self(), :working_working, 1000)
+    time = Time.add(time, 1)
+
+    # Check if snooze alarm should trigger again
+    if time == alarm do
+      IO.puts("SNOOZE ALARM TRIGGERED AGAIN!")
+      :gproc.send({:p, :l, :ui_event}, :start_alarm)
+      GenServer.cast(ui, :set_indiglo)
+      Process.send_after(self(), :toggle_indiglo, 500)
+      state = %{state | st: :alarm_on, indiglo_count: 0, time: time}
+      GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string()})
+      {:noreply, state}
+    else
+      GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string()})
+      {:noreply, %{state | time: time}}
+    end
+  end
+
 end
